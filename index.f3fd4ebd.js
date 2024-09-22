f69ebd1cd91ce29c132b223f760d78e599f94aa5
Kostya Vasilenko <kostyantin.vasilenko.03@gmail.com>
2024-09-22 13:21:21 +0300

    make build

---
+++ index.f3fd4ebd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.querySelector(".spider");e.style.position="relative",t.style.position="absolute",t.style.top="50%",t.style.left="50%",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.f3fd4ebd.js.map
